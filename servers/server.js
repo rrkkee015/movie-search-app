f163226a0077c48245baa9d9869fafc5b2e188e3
GGODONG <pok_winter@naver.com>
2021-01-16 21:30:44 +0900

    connect mongodb

--- servers/server.js
+++ servers/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 // const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3001;
 const route = require('./routes/index');
 
-// app.use(cors());
+// DB setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once('open', function(){
+  console.log('DB connected');
+});
+db.on('error', function(err){
+  console.log('DB ERROR : ', err);
+});
 
+// app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', route);
 
